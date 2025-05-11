2d5675b9f490fc5e720b03ad3815f7f861c22618
DhanushikaGit <danushikam804@gmail.com>
2025-05-11 09:25:14 +0530

    refactor: update swiper dependencies and enhance HeroSlide component functionality

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,3 +1,8 @@
-import { apiConfig } from "./axiosClient";
+const apiConfig = {
+  baseUrl: 'https://api.themoviedb.org/3/',
+  apiKey: '7d63737a96196b58b70eb2c2c85bf928', // Replace with your actual TMDB API key
+  originalImage: (imgPath) => `https://image.tmdb.org/t/p/original${imgPath}`,
+  w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500${imgPath}`
+};
 
 export default apiConfig;
