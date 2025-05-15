715da8e7b2922e8ad5f4cef1110fc0eb4eafdc5c
DhanushikaGit <danushikam804@gmail.com>
2025-05-15 14:41:04 +0530

    refactor: improve HeroSlide component structure and functionality, update Button component usage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import 'swiper/swiper.min.css';
+import 'swiper/css'; // Correct Swiper CSS import
 import './assets/boxicons-2.0.7/css/boxicons.min.css';
 import './App.scss';
 
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
-
 import RoutesConfig from './config/Routes';
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="*" element={
-                    <>
-                        <Header />
-                        <RoutesConfig />
-                        <Footer />
-                    </>
-                } />
+                <Route
+                    path="*"
+                    element={
+                        <>
+                            <Header />
+                            <RoutesConfig />
+                            <Footer />
+                        </>
+                    }
+                />
             </Routes>
         </BrowserRouter>
     );
